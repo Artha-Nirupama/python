bf93a853365589666ffd029d3ed29416e0d63694
G.M.Artha <arthanirupama3.o@gmail.com>
2025-09-21 08:48:26 +0530

    1st one

---
+++ PyQt5F/act1.py
@@ -0,0 +1,30 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QPushButton,QVBoxLayout,QHBoxLayout
+
+app=QApplication([])
+window=QWidget()
+
+window.setWindowTitle("The First One")
+window.resize(200,200)
+
+text1 = QLabel("Hello World!")
+text2 = QLabel("Apple")
+text3 = QLabel("Banana")
+text4 = QLabel("Mango")
+
+master_layout = QVBoxLayout()
+row1 = QHBoxLayout()
+row2 = QHBoxLayout()
+
+row1.addWidget(text1,alignment=Qt.AlignCenter)
+row2.addWidget(text2,alignment=Qt.AlignCenter)
+row2.addWidget(text3,alignment=Qt.AlignCenter)
+row2.addWidget(text4,alignment=Qt.AlignCenter)
+
+master_layout.addLayout(row1)
+master_layout.addLayout(row2)
+
+window.setLayout(master_layout)
+
+window.show()
+app.exec_()
