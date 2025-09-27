caabb6922213b07c4242eb1505aec356b85f3042
G.M.Artha <arthanirupama3.o@gmail.com>
2025-09-27 23:49:53 +0530

    chalanges

---
+++ 12.py/chalange1.py
@@ -0,0 +1,4 @@
+words = ["sun", "moon", "star", "sky"]
+
+numWords = [f"{x}-{i}" for x,i in enumerate(words)]
+print(numWords)
---
+++ 12.py/chalange3.py
@@ -0,0 +1,4 @@
+animals = ["cat", "dog", "rabbit", "tiger"]
+
+dic = {x:i for i,x in enumerate(animals)}
+print(dic)
