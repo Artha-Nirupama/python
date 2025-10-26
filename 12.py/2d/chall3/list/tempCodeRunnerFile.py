2bbe59d4b16d95493efd88cfedc4e4da1eee180b
G.M.Artha <arthanirupama3.o@gmail.com>
2025-10-26 07:20:21 +0530

    tidying up

---
+++ 12.py/2d/chall3/list/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+matrix = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+outMatrix = [[x,x+3,x+6] for x in range(1,4)]
+print(outMatrix)
---
+++ 12.py/2d/chall4/q1.py
@@ -0,0 +1,3 @@
+see = [[3*x,3*x-1,3*x-2] for x in range(3,0,-1)]
+
+print(see)
